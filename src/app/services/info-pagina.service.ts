50edf1fa87e7f653ae5f262c548c950d853259f5
Junior Avila <junioravila1@gmail.com>
2020-03-19 02:40:27 -0500

    Firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,14 +8,24 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: any[] = [];
   cargada = false;
   constructor(private http: HttpClient) {
-    console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
+      })
+  }
+  private cargarEquipo() {
+    this.http.get('https://angular-html-e2928.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
         console.log(resp);
       })
   }
